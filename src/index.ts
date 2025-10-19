055cd65ee3dd656f5b60bcbb7140fbf4c53ecdbe
r64 <202770393+ruka-64@users.noreply.github.com>
2025-10-19 23:29:45 +0000

    idk why this code is working

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import { createBot } from "mineflayer"
+import { mineflayer as mineflayerViewer } from "prismarine-viewer"
+
+
+const bot = createBot({
+  username: 'Bot',
+  host: "marvgame.net",
+  port: 25565,
+  auth: "microsoft"
+})
+
+bot.once('spawn', () => {
+  mineflayerViewer(bot, { port: 3000 }) // Start the viewing server on port 3000
+
+  // Draw the path followed by the bot
+  const path = [bot.entity.position.clone()]
+  bot.on('move', () => {
+    if (path[path.length - 1].distanceTo(bot.entity.position) > 1) {
+      path.push(bot.entity.position.clone())
+      bot.viewer.drawLine('path', path)
+    }
+  })
+})
